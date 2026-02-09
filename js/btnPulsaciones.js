bab6447a5bd899ae9e61930aeb737cf18c24328c
APLUniverso <juanfelixbd@gmail.com>
2026-02-09 17:27:33 -0500

    feat: :sparkles: Funcionamiento del boton terminado

--- js/btnPulsaciones.js
+++ js/btnPulsaciones.js
@@ -1,10 +1,19 @@
 const btn = document.getElementById("morse-btn");
-const salida = document.getElementById("salida");
+const morseSalida = document.getElementById("morse");
+const textoSalida = document.getElementById("texto");
+const espacio = document.getElementById("espacio");
+
+const PAUSA_LETRA = 800;
+const UMBRAL = 300;
 
 let tiempoInicio = 0;
-const UMBRAL = 300; // ms (ajustable)
+let temporizadorLetra = null;
 
 function iniciar() {
+  if (temporizadorLetra) {
+    clearTimeout(temporizadorLetra);
+  }
+
   tiempoInicio = Date.now();
 }
 
@@ -12,16 +21,59 @@ function terminar() {
   const duracion = Date.now() - tiempoInicio;
 
   if (duracion < UMBRAL) {
-    salida.textContent += "·";
+    morseSalida.textContent += ".";
   } else {
-    salida.textContent += "—";
+    morseSalida.textContent += "-";
   }
+
+  temporizadorLetra = setTimeout(() => {
+    traducir();
+  }, PAUSA_LETRA);
 }
 
-// PC
+function traducir() {
+  const morse = morseSalida.textContent;
+  if (morse === ".-") {textoSalida.textContent += "A";}
+  else if (morse === "-...") { textoSalida.textContent += "B";}
+  else if (morse === "-.-.") { textoSalida.textContent += "C";}
+  else if (morse === "-..")  { textoSalida.textContent += "D";}
+  else if (morse === ".")    { textoSalida.textContent += "E";}
+  else if (morse === "..-.") { textoSalida.textContent += "F";}
+  else if (morse === "--.")  { textoSalida.textContent += "G";}
+  else if (morse === "....") { textoSalida.textContent += "H";}
+  else if (morse === "..")   { textoSalida.textContent += "I";}
+  else if (morse === ".---") { textoSalida.textContent += "J";}
+  else if (morse === "-.-")  { textoSalida.textContent += "K";}
+  else if (morse === ".-..") { textoSalida.textContent += "L";}
+  else if (morse === "--")   { textoSalida.textContent += "M";}
+  else if (morse === "-.")   { textoSalida.textContent += "N";}
+  else if (morse === "---")  { textoSalida.textContent += "O";}
+  else if (morse === ".--.") { textoSalida.textContent += "P";}
+  else if (morse === "--.-") { textoSalida.textContent += "Q";}
+  else if (morse === ".-.")  { textoSalida.textContent += "R";}
+  else if (morse === "...")  { textoSalida.textContent += "S";}
+  else if (morse === "-")    { textoSalida.textContent += "T";}
+  else if (morse === "..-")  { textoSalida.textContent += "U";}
+  else if (morse === "...-") { textoSalida.textContent += "V";}
+  else if (morse === ".--")  { textoSalida.textContent += "W";}
+  else if (morse === "-..-") { textoSalida.textContent += "X";}
+  else if (morse === "-.--") { textoSalida.textContent += "Y";}
+  else if (morse === "--..") { textoSalida.textContent += "Z";}
+  else {textoSalida.textContent += "";}
+
+  morseSalida.textContent = "";
+}
+
+function putEspacio(){
+  textoSalida.textContent += " ";
+}
+
+// Eventos PC
 btn.addEventListener("mousedown", iniciar);
 btn.addEventListener("mouseup", terminar);
 
-// Celular
+espacio.addEventListener("click", putEspacio);
+
+// Eventos móvil
 btn.addEventListener("touchstart", iniciar);
 btn.addEventListener("touchend", terminar);
